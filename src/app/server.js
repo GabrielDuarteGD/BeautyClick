f140428dbbd7b0514149c868bc392ad6582f5477
e_gduarte <gabriel.duarte@ayi-asociados.com>
2019-11-22 22:17:06 -0300

    heroku deploy

--- src/app/server.js
+++ src/app/server.js
@@ -13,5 +13,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default Heroku PORT
 app.listen(process.env.PORT || 8080);
